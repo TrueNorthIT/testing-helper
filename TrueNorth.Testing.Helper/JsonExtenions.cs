a7702abca6a91313f91e4aaec083f844e0c81528
Steve Drake <sdrake@trueNorthIT.co.uk>
2018-02-05 10:54:58 +0000

    JSON extenion and myfact

---
+++ TrueNorth.Testing.Helper/JsonExtenions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TrueNorth.Testing.Helper
+{
+    public static class JsonExtenions
+    {
+        public static string ToJsonString(this object o)
+        {
+            return JsonConvert.SerializeObject(o, Formatting.Indented);
+        }
+    }
+}
---
+++ TrueNorth.Testing.Helper/MyFactAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace TrueNorth.Testing.Helper
+{
+    public sealed class MyFactAttribute : Xunit.FactAttribute
+    {
+        public MyFactAttribute([CallerMemberName] string memberName = null)
+        {
+            DisplayName = memberName;
+        }
+    }
+}
