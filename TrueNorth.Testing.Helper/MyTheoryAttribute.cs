8123926cb8ac0ee1208180f51ba10474f26a4547
Steve Drake <sdrake@trueNorthIT.co.uk>
2018-03-16 11:33:14 +0000

    added Displayname support for Theory

---
+++ TrueNorth.Testing.Helper/MyTheoryAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TrueNorth.Testing.Helper
+{
+    public class MyTheoryAttribute :  Xunit.TheoryAttribute
+    {
+        public MyTheoryAttribute([CallerMemberName] string memberName = null)
+        {
+            DisplayName = memberName;
+        }
+    }
+}
